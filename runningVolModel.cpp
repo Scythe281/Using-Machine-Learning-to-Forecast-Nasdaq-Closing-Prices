e755ca629fef83fa79ff9bd9f573d38ef562ea5f
RoRiRe <67814069+Scythe281@users.noreply.github.com>
2025-11-19 22:31:46 -0600

    Add files via upload

--- runningVolModel.cpp
+++ runningVolModel.cpp
@@ -24,9 +24,31 @@ std::vector<std::pair<std::string, std::vector<double>>> data = myReader.read_CS
 std::vector<double> openingPrices = data[0].second;
 std::vector<double> closingPrices = data[1].second;
 
-ewmaVolModel myEWMA(closingPrices);
+//constructor to feed Nasdaq Five year daily data
+ewmaVolModel myCloseEWMA(closingPrices);
+ewmaVolModel myOpenEWMA(openingPrices);
 
-double varienceT = myEWMA.EWMA();
+// Calculating log returns + returns squared for EWMA
+myCloseEWMA.lnReturn();
+myOpenEWMA.lnReturn();
+
+std::cout << "Hello!\nThis little project aims to show the evolution of varience using the EWMA Volatility Model.\nThe project's data comes from historical Nasdaq prices starting at 2020-11-13 and ending at 2025-11-12.\nThe initial varience is set as the average of the first 500 log returns squared.\n" << std::endl;
+
+std::cout << "Starting EWMA Volatility for NASDAQ Close Prices . . . . .\n" << std::endl;
+double varienceClose = myCloseEWMA.EWMA();
+
+std::cout << " . . . . . Ending Close Prices EWMA\n" << std::endl;
+std::cout << "Starting EWMA Volatility for NASDAQ Open Prices . . . . .\n" << std::endl;
+
+double varienceOpen = myOpenEWMA.EWMA();
+std::cout << " . . . . Ending Open Prices EWMA\n" << std::endl;
+
+std::cout << " ========================================== " << "\n" << std::endl;
+
+
+std::cout << "Ending EWMA using closing prices is: " << varienceClose << "\n" << std::endl;
+
+std::cout << "Ending EWMA using opening prices is: "  << varienceOpen << "\n" << std::endl;
 
 
 
@@ -34,24 +56,3 @@ double varienceT = myEWMA.EWMA();
         
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
